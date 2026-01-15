40f56025f8fef51a995006e55b50ca9bc8b41434
Marco Barozzi <mbzstudent@gmail.com>
2026-01-15 07:22:42 -0300

    desenvolvendo onBoardingScreen parte 1

--- lib/view/onboarding_screen.dart
+++ lib/view/onboarding_screen.dart
@@ -1,3 +1,4 @@
+import 'package:ecom_2026/utils/app_textstyles.dart';
 import 'package:flutter/material.dart';
 
 class OnboardingScreen extends StatefulWidget {
@@ -8,8 +9,113 @@ class OnboardingScreen extends StatefulWidget {
 }
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
+  final PageController _pageController = PageController();
+  int _currentPage = 0;
+
+  final List<OnBoardingItem> _items = [
+    OnBoardingItem(
+      image: 'assets/images/intro.png',
+      title: 'Discover Last Trends',
+      description:
+          'Explore the newest fashion trends and find your unique style',
+    ),
+    OnBoardingItem(
+      image: 'assets/images/intro1.png',
+      title: 'Quality Products',
+      description: 'Shop premiun quality products from brands worldwide',
+    ),
+    OnBoardingItem(
+      image: 'assets/images/intro2.png',
+      title: 'Easy Shopping',
+      description: 'Simple and secure shopping experience at your fingertips',
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('Onboarding screen')));
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          PageView.builder(
+            controller: _pageController,
+            itemCount: _items.length,
+            onPageChanged: (index) {
+              setState(() {
+                _currentPage = index;
+              });
+            },
+            itemBuilder: (context, index) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    _items[index].image,
+                    height: MediaQuery.of(context).size.height * 0.4,
+                  ),
+                  const SizedBox(height: 40),
+                  Text(
+                    _items[index].title,
+                    textAlign: TextAlign.center,
+                    style: AppTextStyle.withColor(
+                      AppTextStyle.h1,
+                      Theme.of(context).textTheme.bodyLarge!.color!,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 32),
+                    child: Text(
+                      _items[index].description,
+                      textAlign: TextAlign.center,
+                      style: AppTextStyle.withColor(
+                        AppTextStyle.bodyLarge,
+                        isDark ? Colors.grey[400]! : Colors.grey[600]!,
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
+          Positioned(
+            bottom: 80,
+            left: 0,
+            right: 0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List.generate(
+                _items.length,
+                (index) => AnimatedContainer(
+                  duration: Duration(microseconds: 300),
+                  margin: EdgeInsets.symmetric(horizontal: 4),
+                  height: 8,
+                  width: _currentPage == index ? 24 : 8,
+                  decoration: BoxDecoration(
+                    color: _currentPage == index
+                        ? Theme.of(context).primaryColor
+                        : (isDark ? Colors.grey[700] : Colors.grey[300]),
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
+}
+
+class OnBoardingItem {
+  final String image;
+  final String title;
+  final String description;
+
+  OnBoardingItem({
+    required this.image,
+    required this.title,
+    required this.description,
+  });
 }
