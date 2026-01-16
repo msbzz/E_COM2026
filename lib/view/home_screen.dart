5900ac45c32b37ab720d436468ded81b290063c3
Marco Barozzi <mbzstudent@gmail.com>
2026-01-16 17:07:12 -0300

    authentication screens parte 7 - Home screen part 1

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,10 +1,73 @@
+import 'package:ecom_2026/controllers/theme_controller.dart';
+import 'package:ecom_2026/view/widgets/custom_search_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('Home Screen')));
+    return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    radius: 20,
+                    backgroundImage: AssetImage('assets/images/avatar.jpg'),
+                  ),
+                  SizedBox(height: 12),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Hello Marco',
+                        style: TextStyle(color: Colors.grey, fontSize: 14),
+                      ),
+                      Text(
+                        'Good Morning',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Spacer(),
+                  // notification icon
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.notifications_outlined),
+                  ),
+                  // car button
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.shopping_bag_outlined),
+                  ),
+                  // theme button
+                  GetBuilder<ThemeController>(
+                    builder: (controller) => IconButton(
+                      onPressed: () => controller.toggleTheme(),
+                      icon: Icon(
+                        controller.isDarkMode
+                            ? Icons.light_mode
+                            : Icons.dark_mode,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // search bar
+            const CustomSearchBar(),
+          ],
+        ),
+      ),
+    );
   }
 }
