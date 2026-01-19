16b823fc96e64975f236f1c4dddf4c8b2d7df202
Marco Barozzi <mbzstudent@gmail.com>
2026-01-19 15:39:44 -0300

    WishList 27 - parte 1

--- lib/view/wishlist_screen.dart
+++ lib/view/wishlist_screen.dart
@@ -1,3 +1,5 @@
+import 'package:ecom_2026/models/product.dart';
+import 'package:ecom_2026/utils/app_textstyles.dart';
 import 'package:flutter/material.dart';
 
 class WishlistScreen extends StatelessWidget {
@@ -5,6 +7,55 @@ class WishlistScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('Wishlist Screen')));
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+
+    return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      appBar: AppBar(
+        title: Text(
+          'My Wishlist',
+          style: AppTextStyle.withColor(
+            AppTextStyle.h3,
+            isDark ? Colors.white : Colors.black,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.search,
+              color: isDark ? Colors.white : Colors.black,
+            ),
+          ),
+        ],
+      ),
+      body: CustomScrollView(
+        slivers: [SliverToBoxAdapter(child: _buildSummarySection(context))],
+      ),
+    );
+  }
+
+  Widget _buildSummarySection(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+    final favoriteProducts = products.where((p) => p.isFavorite).length;
+
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: isDark ? Colors.grey[850] : Colors.grey[100],
+        borderRadius: const BorderRadius.vertical(bottom: Radius.circular(16)),
+      ),
+      child: Row(
+        children: [
+          Text(
+            '$favoriteProducts Items',
+            style: AppTextStyle.withColor(
+              AppTextStyle.h2,
+              Theme.of(context).textTheme.bodyLarge!.color!,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
