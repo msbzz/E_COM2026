0b1d6fe7bec25c1867687591ce359b1d4e0811a4
Marco Barozzi <mbzstudent@gmail.com>
2026-01-23 14:59:47 -0300

    my orders screen 52 - parte 3

--- lib/features/my_orders/view/widgets/order_cart.dart
+++ lib/features/my_orders/view/widgets/order_cart.dart
@@ -1,4 +1,5 @@
 import 'package:ecom_2026/features/my_orders/model/order.dart';
+import 'package:ecom_2026/utils/app_textstyles.dart';
 import 'package:flutter/material.dart';
 
 class OrderCart extends StatelessWidget {
@@ -39,7 +40,25 @@ class OrderCart extends StatelessWidget {
                   height: 80,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(12),
-                    image: DecorationImage(image: AssetImage(order.imageUrl)),
+                    image: DecorationImage(
+                      image: AssetImage(order.imageUrl),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 16),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Order # ${order.orderNumber}',
+                        style: AppTextStyle.withColor(
+                          AppTextStyle.h3,
+                          Theme.of(context).textTheme.bodyLarge!.color!,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
