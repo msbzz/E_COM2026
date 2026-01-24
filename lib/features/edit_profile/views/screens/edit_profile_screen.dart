d08115ac65bfec1df256ad617a52f0a446c032ef
Marco Barozzi <mbzstudent@gmail.com>
2026-01-24 17:37:03 -0300

    edit profile screen 74 - parte 2

--- lib/features/edit_profile/views/screens/edit_profile_screen.dart
+++ lib/features/edit_profile/views/screens/edit_profile_screen.dart
@@ -1,5 +1,7 @@
+import 'package:ecom_2026/features/edit_profile/views/widgets/profile_image.dart';
 import 'package:ecom_2026/utils/app_textstyles.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class EditProfileScreen extends StatelessWidget {
   const EditProfileScreen({Key? key}) : super(key: key);
@@ -9,6 +11,10 @@ class EditProfileScreen extends StatelessWidget {
     final isDark = Theme.of(context).brightness == Brightness.dark;
     return Scaffold(
       appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => Get.back(),
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
         title: Text(
           'Edit Profile',
           style: AppTextStyle.withColor(
@@ -16,6 +22,9 @@ class EditProfileScreen extends StatelessWidget {
             isDark ? Colors.white : Colors.black,
           ),
         ),
+      ),
+      body: const SingleChildScrollView(
+        child: Column(children: [SizedBox(height: 24), ProfileImage()]),
       ),
     );
   }
---
+++ lib/features/edit_profile/views/widgets/profile_image.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class ProfileImage extends StatelessWidget {
+  const ProfileImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+    return Center(
+      child: Stack(
+        children: [
+          Container(
+            width: 120,
+            height: 120,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(
+                color: Theme.of(context).primaryColor,
+                width: 2,
+              ),
+              image: DecorationImage(
+                image: AssetImage('assets/images/avatar.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: GestureDetector(
+              onTap: () {},
+              child: Container(
+                padding: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor,
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                      color: isDark
+                          ? Colors.black.withValues(alpha: 0.2)
+                          : Colors.grey.withValues(alpha: 0.1),
+                      blurRadius: 8,
+                      offset: const Offset(0, 2),
+                    ),
+                  ],
+                ),
+                child: const Icon(
+                  Icons.camera_alt,
+                  color: Colors.white,
+                  size: 20,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
