60f4ae3c03d4de2eb712360662e217a8651bd796
Marco Barozzi <mbzstudent@gmail.com>
2026-01-25 07:17:10 -0300

    edit profile screen 79 - parte 7

--- lib/features/edit_profile/views/widgets/profile_form.dart
+++ lib/features/edit_profile/views/widgets/profile_form.dart
@@ -1,5 +1,7 @@
 import 'package:ecom_2026/features/widgets/custom_textField.dart';
+import 'package:ecom_2026/utils/app_textstyles.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ProfileForm extends StatelessWidget {
   const ProfileForm({Key? key}) : super(key: key);
@@ -51,6 +53,50 @@ class ProfileForm extends StatelessWidget {
               prefixIcon: Icons.mail_outlined,
               initialValue: 'alexJohnson@mmmm.com',
               keyboardType: TextInputType.emailAddress,
+            ),
+          ),
+          const SizedBox(height: 16),
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).cardColor,
+              borderRadius: BorderRadius.circular(12),
+              boxShadow: [
+                BoxShadow(
+                  color: isDark
+                      ? Colors.black.withValues(alpha: 0.2)
+                      : Colors.grey.withValues(alpha: 0.1),
+                  blurRadius: 8,
+                  offset: Offset(0, 2),
+                ),
+              ],
+            ),
+            child: CustomTextField(
+              label: 'Phone Number',
+              prefixIcon: Icons.phone_outlined,
+              initialValue: '55(31)929xxxx7',
+              keyboardType: TextInputType.phone,
+            ),
+          ),
+          const SizedBox(height: 32),
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () => Get.back(),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Theme.of(context).primaryColor,
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                elevation: 0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+              child: Text(
+                'Save Change',
+                style: AppTextStyle.withColor(
+                  AppTextStyle.buttonMedium,
+                  Colors.white,
+                ),
+              ),
             ),
           ),
         ],
