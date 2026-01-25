1c427bfdcd60acd3a8d4b881ad8c9d2f209873f1
Marco Barozzi <mbzstudent@gmail.com>
2026-01-25 07:36:40 -0300

    help center screen 89 - parte 2

--- lib/features/help_center/views/screens/help_center_screen.dart
+++ lib/features/help_center/views/screens/help_center_screen.dart
@@ -1,5 +1,6 @@
 import 'package:ecom_2026/utils/app_textstyles.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HelpCenterScreen extends StatelessWidget {
   const HelpCenterScreen({Key? key}) : super(key: key);
@@ -8,7 +9,15 @@ class HelpCenterScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final isDark = Theme.of(context).brightness == Brightness.dark;
     return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => Get.back(),
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: isDark ? Colors.white : Colors.black,
+          ),
+        ),
         title: Text(
           'Help Center',
           style: AppTextStyle.withColor(
