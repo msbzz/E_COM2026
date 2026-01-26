08780971941320eda1337a92130750419172d520
Marco Barozzi <mbzstudent@gmail.com>
2026-01-26 19:13:41 -0300

    settings privacy 90 parte 9

--- lib/features/checkout/view/screens/checkout_screen.dart
+++ lib/features/checkout/view/screens/checkout_screen.dart
@@ -1,10 +1,30 @@
+import 'package:ecom_2026/utils/app_textstyles.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class CheckoutScreen extends StatelessWidget {
   const CheckoutScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => Get.back(),
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: isDark ? Colors.white : Colors.black,
+          ),
+        ),
+        title: Text(
+          'Check',
+          style: AppTextStyle.withColor(
+            AppTextStyle.h3,
+            isDark ? Colors.white : Colors.black,
+          ),
+        ),
+      ),
+    );
   }
 }
